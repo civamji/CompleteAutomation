047e927c633c93e1381858aa146d108835e0921a
shivam sharma <civamofficial@gmail.com>
2025-01-18 04:33:01 -0800

    Added Skyscanner flight book tstcase

--- src/test/java/SkyScanner.java
+++ src/test/java/SkyScanner.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SkyScanner {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void BookaTicket() throws InterruptedException {
+        driver.get("https://www.skyscanner.co.in/");
+        driver.wait(5000);
+        driver.findElement(By.id("destinationInput-input")).sendKeys("Dubai");
+        driver.wait(5000);
+        driver.findElement(By.className("SearchControlButton_SearchControlBtn__YzUyO SearchControlButton_DesktopBtn__NzM1Z")).click();
+        driver.findElement(By.xpath("//button[@aria-label='Saturday, 18 January 2025. Select as departure date']")).click();
+        driver.wait(5000);
+        driver.findElement(By.xpath("//button[@aria-label='Sunday, 19 January 2025. Select as departure date']")).click();
+        driver.wait(5000);
+        driver.findElement(By.xpath("//button[normalize-space()='Search']")).click();
+    }
 }
--- src/test/java/models/scrollTillElement.java
+++ src/test/java/models/scrollTillElement.java
@@ -1,4 +1,36 @@
 package models;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class scrollTillElement {
+
+    public static void main(String[] args) throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver",
+//                "C:\Users\ghs6kor\Desktop\Java\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        String url = "https://www.tutorialspoint.com/scroll-element-into-view-with-selenium";
+        driver.get(url);
+        Thread.sleep(5000);
+       // driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
+        // identify element
+        WebElement element=driver.findElement(By.xpath("//li[normalize-space()='Related Articles']"));
+
+        // Javascript executor to scroll page till element location
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
+        //JavaScript executor to scroll page till bottom of the page
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+
+        driver.quit();
+
+    }
+
 }
